70b415b2403cd47dc43df4f26ae8fe75d8bc0c48
Sebastian Rodriguez <sebaser99@gmail.com>
2024-11-05 15:56:44 -0500

    creamos componente ejercicio3

---
+++ src/app/components/signal-ejemplo-3/signal-ejemplo-3.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SignalEjemplo3Component } from './signal-ejemplo-3.component';
+
+describe('SignalEjemplo3Component', () => {
+  let component: SignalEjemplo3Component;
+  let fixture: ComponentFixture<SignalEjemplo3Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SignalEjemplo3Component]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SignalEjemplo3Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
